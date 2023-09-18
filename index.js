1937cfb4afd9be49aab8f8d34ece16b9480e258a
vmadhwal <vaibhavmadhwal123@gmail.com>
2023-09-18 22:05:15 +0530

    innitial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var btn= document.getElementById("btn");
+var lit=document.getElementById("light"); 
+function toggleBtn(){
+    btn.classList.toggle("active");
+    lit.classList.toggle("on");
+
+}
